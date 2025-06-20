ce89b0144c0861bf2c21ededb198ca1c0679befa
Mark Suder <mark.d.suder@nasa.gov>
2025-06-20 13:48:33 -0400

    https://github.com/nasa/nos3/issues/300 - Eliminate dead code and update blackboard fields.

--- inc/blackboard_data.hpp
+++ inc/blackboard_data.hpp
@@ -19,20 +19,32 @@ namespace Nos3
 
     struct BlackboardData {
         bip::interprocess_mutex mutex;
+        double qn[4];
+        double wn[3];
         double svb[3];
         double bvb[3];
         double Hvb[3];
+        double WhlH[3];
         double GyroRate[3];
+        double MagField[3];
         int    CSSValid[6];
         double CSSIllum[6];
         int    FSSValid;
         double FSSSunAng[2];
         int    STValid;
         double STqn[4];
+        int    GPSValid;
+        int    GPSRollover;
+        int    GPSWeek;
+        double GPSSec;
         double GPSPosN[3];
         double GPSVelN[3];
+        double GPSPosW[3];
+        double GPSVelW[3];
+        double GPSLng;
+        double GPSLat;
+        double GPSAlt;
         double AccelAcc[3];
-        double WhlH[3];
     };
 
 }
